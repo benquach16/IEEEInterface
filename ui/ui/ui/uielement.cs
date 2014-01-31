fbf9382578d3c58bffd902bacf9e6eb13fd9a3c2
Benjamin Quach <benquach16@yahoo.com>
2014-01-31 00:05:55 -0800

    inital commit

---
+++ ui/ui/ui/uielement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace ui
+{
+    //abstract base class
+    class UIElement
+    {
+        enum E_UI_TYPES
+        {
+            //for oop
+
+        };
+        //setup class hierarchy
+        protected Vector2 position;
+        protected Vector2 size;
+
+        UIElement()
+        {
+            
+        }
+
+        Vector2 getSize()
+        {
+            return size;
+        }
+    }
+}
